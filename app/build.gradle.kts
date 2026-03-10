930dd8704f722dc97e361ba996156577911acc1e
Marco <marco20001114@gmail.com>
2026-03-10 16:12:12 +0800

    chore: add material icons extended dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation(libs.androidx.material3)
     testImplementation(libs.junit)
